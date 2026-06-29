7e997e580c65c196f376fc3ad347a2ba0de47829
seyam-ali <seyamhossain482@gmail.com>
2026-06-29 12:32:34 +0600

    feat: configure server-side rendering routes and increase bundle size budget limits

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,14 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'match/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'watch/:id',
+    renderMode: RenderMode.Server
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
